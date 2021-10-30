c0f4f337c2acc154d6e7ab4b6df844c74199e7cb
HZR_H3RY <79120962+hafiz573@users.noreply.github.com>
2021-10-30 11:21:23 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js'),
+client = new Discord.Client(),
+const config = require("config.json")
+Got = require('got');
+const fetch = require('node-fetch');
+require('discord-reply'),
+require('./server')
+
+client.on('ready', ()=>{
+  console.log(`${client.user.tag} is ready to chat with ${client.users.cache.size} people`)
+client.user.setActivity("Chatting with people", {
+  type: "STREAMING",
+  url: "https://www.twitch.com/yassinehz"
+});
+});
+
+
+
+client.on('message', async message =>{
+  if(message.channel.type ==="dm") {
+    if(message.author.bot) return;
+  message.channel.startTyping();
+
+if (!message.content) return message.channel.send("Please say something.");
+await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${client.user.username}&ownername=DEVELOPER_NAME`)
+    .then(res => res.json())
+    .then(data => {
+        message.lineReplyNoMention({embed : {
+          description : `${data.message}`,
+          color : `#2f3136`
+        }});
+    });
+      message.channel.stopTyping();
+
+}
+});
+
+client.login(config.token) //<3
